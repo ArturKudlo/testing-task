f88a477c8c086ccdee3aa009c1870ed3dcaaba8c
arturkudli <arturkapes@gmail.com>
2025-03-22 22:49:52 +0300

    docs: add fonts,images,icons; add styles base

---
+++ src/js/constants.js
@@ -0,0 +1,15 @@
+export const APP_CONTENT_VALUES = {
+  'get-unlimited-access': 'Get Unlimited <br>Access',
+  'unlimited-art-creation': 'Unlimited Art <br>Creation',
+  'exclusive-styles': 'Exclusive <br>Styles',
+  'magic-avatars': 'Magic Avatars <br>With 20% Off',
+  'best-offer': 'BEST OFFER',
+  'yearly-access': 'YEARLY ACCESS',
+  'price-year': 'Just {{price}} per year',
+  'price-week': '{{price}} <br>per week',
+  'weekly-access': 'WEEKLY ACCESS',
+  continue: 'Continue',
+  'terms-of-use': 'Terms of Use',
+  policy: 'Privacy Policy',
+  restore: 'Restore',
+};
